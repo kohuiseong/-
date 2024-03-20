89543dd4a9c6f4abbbbb8c97e8ec9afe80b0936a
kohuiseong <gmltjd904@gmail.com>
2024-03-20 20:57:31 +0900

    feat: 변수 선언

--- src/variable/Var1.java
+++ src/variable/Var1.java
@@ -1,2 +1,9 @@
-package variable;public class Var1 {
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+    }
 }
