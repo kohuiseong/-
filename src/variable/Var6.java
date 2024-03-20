9c0fec1ecad13d46ed3eac34ed18d5f378fa555f
kohuiseong <gmltjd904@gmail.com>
2024-03-20 21:11:19 +0900

    feat: 변수 초기화 해야 하는 이유

---
+++ src/variable/Var6.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        /*
+        * 변수 초기화를 하지 않으면 컴퓨터에서 메모리는 여러 시스템이 함께 사용하는 공간이다. 그래서 어떠한 값들이 계속해서 저장 된다.
+        * 변수를 선언하면 메모리상의 어떤 공간을 차지하고 사용한다. 그런데 그 공간에 기존에 어떤 값이 있었는지는 아무도 모른다.
+        * 따라서 초기화를 하지 않으면 이상한 값이 출력 될 수 있다.
+        * */
+        int a;
+//        System.out.println(a);
+    }
+}
