9dabae68e6218247cc67ea9d126d8bab9cf523a6
kohuiseong <gmltjd904@gmail.com>
2024-03-21 16:43:43 +0900

    feat: 변수 문제 풀이

--- src/variable/Var9.java
+++ src/variable/Var9.java
@@ -1,2 +1,19 @@
-package variable;public class Var9 {
+package variable;
+
+public class Var9 {
+    public static void main(String[] args) {
+        /*
+        * 자바 언어의 관례 한번에 정리
+        * 자바에서 클래스 이름의 첫 글자는 대문자로 시작한다. 그리고 나머지는 모두 첫 글자 소문자로 시작한다. 여기에 낙타 표기법을 적용하면 된다.
+        * 클래스 : Person, OrderDetail
+        * 변수를 포함한 나머지 : firstName, userAccount => camel 표기법
+        * 여기에 예외가 딱 2개 있다.
+        * 상수는 모두 대문자를 사용하고 언더바로 구분한다. => USER_LIMIT
+        * 패키지는 모두 소문자를 사용한다. => org.spring.boot
+        *
+        * 참고 : 변수의 이름은 의미있고, 그 용도를 명확하게 설명해야 한다.
+        * a, b : 이런 변수는 용도를 설명하지 않는다. 단순한 예제에서만 사용하는 것이 좋다.
+        * studentCount, maxScore, userAccount, orderCount : 용돋를 명확하게 설명한다.
+        * */
+    }
 }
