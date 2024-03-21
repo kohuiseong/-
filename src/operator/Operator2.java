5ee539c5c76d9e2e7955258f87e206f9e5f17326
kohuiseong <gmltjd904@gmail.com>
2024-03-21 17:14:54 +0900

    feat: 문자열 더하기 수정

--- src/operator/Operator2.java
+++ src/operator/Operator2.java
@@ -1,2 +1,27 @@
-package operator;public class Operator2 {
+package operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+
+        // 문자열과 문자열 더하기1
+        String result1 = "hello " + "world";
+        System.out.println(result1);
+
+        // 문자열과 문자열 더하기2
+        String s1 = "string1";
+        String s2 = "string2";
+        String result2 = s1 + s2;
+        System.out.println(result2);
+
+        // 문자열과 숫자 더하기1
+        String result3 = "a + b = " + "10";
+        System.out.println(result3);
+
+        // 문자열과 숫자 더하기2
+        // 숫자와 문자열을 더하게 되면 숫자는 문자열이 되기 때문에 큰따옴표를 붙여야한다.
+        int num = 20;
+        String str = "a + b = ";
+        String result4 = "a + b = 20";
+        System.out.println(result4);
+    }
 }
