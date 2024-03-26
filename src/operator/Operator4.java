db47164919a2b06d86d57341f4a9b056a8d263ba
kohuiseong <gmltjd904@gmail.com>
2024-03-26 17:03:09 +0900

    feat: 연산자 우선순위

---
+++ src/operator/Operator4.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class Operator4 {
+    public static void main(String[] args) {
+        int sum3 = 2 * 2 + 3 * 3;
+        int sum4 = (2 * 2) + (3 * 3);
+        System.out.println("sum3 = " + sum3);
+        System.out.println("sum4 = " + sum4);
+
+        // sum3, sum4는 결과 값이 같다
+        // 이렇게 복잡한 경우에는 괄호를 넣어주는것이 명확하다. 가독성이 좋고 명확하기 때문에 
+    }
+}
